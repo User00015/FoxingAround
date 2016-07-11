4d150a25365c5f382f5bd75d76487de3458bac8f
User00015 <user00015@gmail.com>
2016-07-11 14:52:49 -0400

    Added front-end buttons to show info, delete, add and subtract monsters.

--- MVC5App/Angular/Encounter/EncounterController.js
+++ MVC5App/Angular/Encounter/EncounterController.js
@@ -23,13 +23,29 @@
     //Defaults
     $scope.difficulty = $scope.difficulties[2]; //Default to Hard
     $scope.toggle = true; //Defaults visible
-    $scope.levels = _.range(1, 20); //Hard-coded character levels
+    $scope.levels = _.range(1, 21); //Hard-coded character levels
 
     var submit = function (params) {
         encounterService.postMonsters(function (encounter) {
             $scope.encounter = encounter;
         }, params);
     };
+
+    $scope.removeMonster = function (id) {
+        $scope.encounter.monsters = _.filter($scope.encounter.monsters, function (monster) {
+            return monster.id !== id;
+        });
+    }
+
+    $scope.addMonster = function (monster) {
+        monster.quantity = monster.quantity + 1;
+    }
+
+    $scope.subtractMonster = function (monster) {
+
+        if (monster.quantity > 1)
+            monster.quantity = monster.quantity - 1; //Don't drop below 1 monster.
+    }
 
     $scope.createEncounters = function () {
         var params = {
