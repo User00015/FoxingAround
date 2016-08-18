ed7b256f0d77aceccf92fc4400e4edff12c478b5
User00015 <user00015@gmail.com>
2016-08-18 16:44:45 -0400

    temp commit. moved table to its own directive

--- MVC5App/Angular/Encounter/Directives/encounterTableDirective.js
+++ MVC5App/Angular/Encounter/Directives/encounterTableDirective.js
@@ -1,12 +1,91 @@
-﻿app.directive('encounterTable', [function () {
+﻿app.directive('encounterTable', ['encounterService', '$modal', '$timeout', 'store', function (encounterService, $modal, $timeout, store) {
     return {
-        restrict: "E",
+        restrict: "AE",
         templateUrl: "Angular/Encounter/Directives/encounterTable.html",
         scope: {
-            encounters: "="
+            encounter: '='
         },
-        link: function (scope) {
+        link: function (scope, element, attr) {
 
+            scope.adjustedDifficulty = "";
+            scope.toggle = true; //Defaults visible
+
+            scope.getMonsterDetails = function (monster) {
+                $modal({
+                    templateUrl: 'Angular/Encounter/Statblock.html',
+                    controller: 'StatblockModalController',
+                    backdrop: true,
+                    resolve: {
+                        monsterId: function () {
+                            return monster.id;
+                        }
+                    }
+                });
+            }
+            var getDifficulty = function (difficulties) {
+                var xp = scope.encounter.difficulty.experienceValue;
+                if (difficulties.easy >= xp) return "Easy";
+                if (difficulties.medium >= xp) return "Medium";
+                if (difficulties.hard >= xp) return "Hard";
+                if (difficulties.deadly >= xp) return "Deadly";
+                return "Deadly++";
+            };
+
+            scope.$watch("encounter", function () {
+                if (_.isUndefined(scope.encounter)) 
+                    return;
+
+                scope.adjustedDifficulty = getDifficulty(scope.encounter.difficulty);
+            });
+
+
+            var updateEncounters = function () {
+                var monstersList = scope.encounter.monsters;
+                encounterService.updateEncounters(function (xp) {
+                    var difficulty = scope.encounter.difficulty;
+                    difficulty.experienceValue = xp;
+                    scope.adjustedDifficulty = getDifficulty(difficulty);
+                }, monstersList);
+            };
+
+            scope.removeMonster = function (monster) {
+                if (monster.quantity > 1) {
+                    monster.quantity = monster.quantity - 1;
+                } else {
+                    scope.encounter.monsters = _.filter(scope.encounter.monsters, function (m) {
+                        return m.id !== monster.id;
+                    });
+                }
+                updateEncounters();
+            }
+
+            scope.addMonster = function (monster) {
+                monster.quantity = monster.quantity + 1;
+                updateEncounters();
+            }
+
+            scope.loadEncounter = function () {
+                var profile = store.get('profile');
+                var params = { email: profile.email }
+
+                encounterService.getSavedEncounters(function (encounter) {
+                    scope.encounter = encounter;
+                    scope.adjustedDifficulty = getDifficulty(encounter.difficulty);
+                }, params);
+            }
+
+            scope.saveEncounter = function () {
+                var profile = store.get('profile');
+                var encounters = _.concat([], scope.encounter);
+
+                var params = {
+                    email: profile.email,
+                    encounters: encounters
+                };
+                encounterService.saveEncounters(function (result) {
+                    console.log("Save Encounter response: " + result.statusText + " " + result.status);
+                }, params);
+            }
         }
     }
 }]);
--- MVC5App/Angular/Encounter/EncounterController.js
+++ MVC5App/Angular/Encounter/EncounterController.js
@@ -1,17 +1,6 @@
 ﻿app.controller('EncounterController', ['$scope', 'encounterService', '$modal', '$timeout', 'store', function ($scope, encounterService, $modal, $timeout, store) {
 
-    $scope.getMonsterDetails = function (monster) {
-        $modal({
-            templateUrl: 'Angular/Encounter/Statblock.html',
-            controller: 'StatblockModalController',
-            backdrop: true,
-            resolve: {
-                monsterId: function () {
-                    return monster.id;
-                }
-            }
-        });
-    }
+
 
     $scope.difficulties = [
 { type: "Easy", value: 0 },
@@ -37,84 +26,11 @@
 
     //Defaults
     $scope.difficulty = $scope.difficulties[2]; //Default to Hard
-    $scope.adjustedDifficulty = "";
-    $scope.toggle = true; //Defaults visible
+
     $scope.levels = _.range(1, 21); //Hard-coded character levels
     $scope.isLoading = false;
     $scope.environment = $scope.environments[0];
 
-
-    var getDifficulty = function (difficulties) {
-        var xp = difficulties.experienceValue;
-        if (difficulties.easy >= xp) return "Easy";
-        if (difficulties.medium >= xp) return "Medium";
-        if (difficulties.hard >= xp) return "Hard";
-        if (difficulties.deadly >= xp) return "Deadly";
-        return "Deadly++";
-    };
-
-    var updateEncounters = function () {
-        var monstersList = $scope.encounter.monsters;
-        encounterService.updateEncounters(function (xp) {
-            var difficulty = $scope.encounter.difficulty;
-            difficulty.experienceValue = xp;
-            $scope.adjustedDifficulty = getDifficulty(difficulty);
-        }, monstersList);
-    };
-
-    var submit = function (params) {
-        encounterService.postMonsters(function (encounter) {
-            $scope.encounter = encounter;
-            $scope.adjustedDifficulty = getDifficulty(encounter.difficulty);
-            $scope.isLoading = false;
-        }, params);
-    };
-
-    var testDelay = function () {
-        $scope.isLoading = true;
-        $timeout(function () {
-            $scope.isLoading = false;
-        }, 2000);
-    }
-
-    $scope.removeMonster = function (monster) {
-        if (monster.quantity > 1) {
-            monster.quantity = monster.quantity - 1;
-        } else {
-            $scope.encounter.monsters = _.filter($scope.encounter.monsters, function (m) {
-                return m.id !== monster.id;
-            });
-        }
-        updateEncounters();
-    }
-
-    $scope.addMonster = function (monster) {
-        monster.quantity = monster.quantity + 1;
-        updateEncounters();
-    }
-
-    $scope.loadEncounter = function () {
-        var profile = store.get('profile');
-        var params = { email: profile.email}
-
-        encounterService.getSavedEncounters(function (encounter) {
-            $scope.encounter = encounter;
-            $scope.adjustedDifficulty = getDifficulty(encounter.difficulty);
-        }, params);
-    }
-
-    $scope.saveEncounter = function() {
-        var profile = store.get('profile');
-        var encounters = _.concat([], $scope.encounter);
-        
-        var params = {
-            email: profile.email,
-            encounters: encounters
-        };
-        encounterService.saveEncounters(function(result) {
-            console.log("Save Encounter response: " + result.statusText+ " " + result.status);
-        }, params);
-    }
 
     $scope.createEncounters = function () {
         $scope.isLoading = true;
@@ -124,6 +40,28 @@
             difficulty: $scope.difficulty.value,
             environment: $scope.environment.value
         };
-        submit(params);
+
+        encounterService.postMonsters(function (encounter) {
+            $scope.encounter = encounter;
+            $scope.isLoading = false;
+        }, params);
     }
+
+    var testDelay = function () {
+        $scope.isLoading = true;
+        $timeout(function () {
+            $scope.isLoading = false;
+        }, 2000);
+    }
+
+    $scope.loadEncounter = function () {
+        var profile = store.get('profile');
+        var params = { email: profile.email }
+
+        encounterService.getSavedEncounters(function (encounter) {
+            $scope.savedEncounters = encounter;
+        }, params);
+    }
+
+
 }]);
