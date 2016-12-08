d2e7c3cda4c71865235d840990871462c6042673
User00015 <user00015@gmail.com>
2016-12-08 14:22:20 -0500

    Moving js files to bower.

--- MVC5App/App_Start/BundleConfig.cs
+++ MVC5App/App_Start/BundleConfig.cs
@@ -14,17 +14,17 @@ namespace MVC5App
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
+                "~/Scripts/bootstrap.min.js",
                 "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                "~/Scripts/angular.js",
-                "~/Scripts/angular-resource.js",
+                "~/bower_components/angular/angular.js",
+                "~/bower_components/angular-resource/angular-resource.js",
                 "~/Scripts/ng-map.min.js",
-                "~/Scripts/angular-environment.js",
+                "~/bower_components/angular-environment/dist/angular-environment.js",
                 "~/Scripts/smart-table.min.js",
-                "~/Scripts/angular-animate.js",
-                "~/Scripts/angular-route.js"));
+                "~/bower_components/angular-animate/angular-animate.min.js",
+                "~/bower_components/angular-route/angular-route.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angularapp").Include(
                 "~/Angular/*.js").IncludeDirectory(
@@ -38,13 +38,13 @@ namespace MVC5App
 
             bundles.Add(new ScriptBundle("~/bundles/extensions").Include(
                 "~/bower_components/angular-responsive-tables/release/angular-responsive-tables.min.js",
-                "~/bower_components/angular-animate/angular-animate.js",
+                "~/bower_components/angular-animate/angular-animate.min.js",
                 "~/bower_components/a0-angular-storage/dist/angular-storage.min.js",
                 "~/bower_components/angular-jwt/dist/angular-jwt.min.js",
                 "~/bower_components/angular-strap/dist/angular-strap.min.js",
                 "~/bower_components/angular-strap/dist/angular-strap.tpl.min.js",
-                "~/bower_components/auth0-lock/build/lock.js",
-                "~/bower_components/angular-lock/dist/angular-lock.js",
+                "~/bower_components/auth0-lock/build/lock.min.js",
+                "~/bower_components/angular-lock/dist/angular-lock.min.js",
                 "~/Scripts/mixins/lodash.js",
                 "~/Scripts/lodash.min.js"));
 
