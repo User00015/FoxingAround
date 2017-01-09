68e921649a8e2a938b9ba9747b38c6267386573a
wayne byard <user00015@gmail.com>
2017-01-08 23:59:58 -0500

    Revert "bad"
    This reverts commit cca0176c4ca24aa21212983540363ec4b475c7ad.

--- MVC5App/bower_components/auth0.js/src/helper/storage/dummy.js
+++
@@ -1,9 +0,0 @@
-function DummyStorage() {}
-
-DummyStorage.prototype.getItem = function (key) { return null; };
-
-DummyStorage.prototype.removeItem = function (key) {};
-
-DummyStorage.prototype.setItem = function (key, value) {};
-
-module.exports = DummyStorage;
